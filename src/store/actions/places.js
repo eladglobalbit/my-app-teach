75e9cce45ab2aed55d9635959cca571ad54ac4bd
Elad Nisan <eladnisan@Elads-MBP.mynet>
2018-11-29 16:34:34 +0200

    send to server data - image + props

--- src/store/actions/places.js
+++ src/store/actions/places.js
@@ -1,15 +1,20 @@
 import { ADD_PLACE, DELETE_PLACE, SELECT_PLACE, DESELECT_PLACE } from './actionTypes';
-
+import { uiStartLoading, uiStopLoading } from './index';
 export const addPlace = (placeName, location, image) => {
 
     return dispatch => {
+        dispatch(uiStartLoading());
         fetch("https://us-central1-appteach-7a364.cloudfunctions.net/storeImage", {
             method: "POST",
             body: JSON.stringify({
                 image: image.base64
             })
         })
-        .catch(err => console.log(err))
+        .catch(err => {
+            console.log(err);
+            alert("Something went wrong, please try again!");
+            dispatch(uiStopLoading());
+        })
         .then(res => res.json())
         .then(parsedRes => {
             const placeData = {
@@ -22,11 +27,16 @@ export const addPlace = (placeName, location, image) => {
                 body: JSON.stringify(placeData)
             })
         })  
-        .catch(err => console.log(err))
+        .catch(err => {
+            console.log(err);
+            alert("Something went wrong, please try again!");
+            dispatch(uiStopLoading());
+        })
         .then(res => res.json())
         .then(parsedRes => {
             console.log(parsedRes);
             alert('done');
+            dispatch(uiStopLoading());
         });
     };
 };
